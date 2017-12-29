401a295791e772ed7b77ae7bafd0ef9c91987057
John Vivian <jtvivian@gmail.com>
2017-12-28 23:57:51 -0800

    Pythonize entry_point syntax

--- twit_bot/setup.py
+++ twit_bot/setup.py
@@ -6,15 +6,12 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
+        'click',
         'rnaseq-lib',
         'pandas',
         'keras',
-        'tensorflow',
         'numpy',
+        'tensorflow'
     ],
-    entry_points='''
-        [console_scripts]
-        train-rnn=train_rnn:train
-    ''',
-)
+    entry_points={
+        'console_scripts': ['train-rnn=train_rnn:train']})
